379458578d2e49057533cc6cb7e4a76f118f4aa0
Abhishek-K-Vishvakarma <gitrepo101@gamil.com>
2024-09-26 20:02:53 +0530

    Add unary Operator

---
+++ unary.js
@@ -0,0 +1,17 @@
+// increment and decrement...
+
+var data = 10;
+var data1 = ++data;
+console.log(data1)
+
+var num1 = 20;
+var num2 = '20';
++num2; // string value convert into number...
+
+console.log(num1 == num2); // true
+console.log(num1 == -num2) // false
+
+let a = false; // 0
+ b = true; // 1
+console.log(+a,+b);
+
