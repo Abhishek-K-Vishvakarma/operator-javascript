41c8dbcc0474014ad729c29274c10ce68ccebfca
Abhishek-K-Vishvakarma <gitrepo101@gamil.com>
2024-09-26 23:57:47 +0530

    Add nothing

--- compare.js
+++ compare.js
@@ -41,22 +41,5 @@ console.log(20 !== '20'); // true
 
 
 
-// class Person {
-//   constructor(name) {
-//     this.name = name;
-//     console.log(new.target.name);
-//   }
-// }
 
-// class Employee extends Person {
-//   constructor(name, title) {
-//     super(name);
-//     this.title = title;
-//   }
-// }
-
-// let john = new Person('John Doe'); // Person
-// let lily = new Employee('Lily Bush', 'Programmer'); // Employee
-// console.log(john);
-// console.log(lily);
 
